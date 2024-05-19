eeb7af9a7a6d7613b482873bd999108a2ca420bb
Junaid Islam <junaid.2005p@gmail.com>
2024-05-19 16:15:05 +0530

    added comments to do.go

--- cmd/do.go
+++ cmd/do.go
@@ -12,36 +12,39 @@ var doCmd = &cobra.Command{
 	Use:   "do",
 	Short: "Marks a given task as completed and deletes it from the list",
 	Run: func(cmd *cobra.Command, args []string) {
-		var ids []int
+		var ids []int // Stores the ids of the tasks to be marked as done in a int slice 'ids'
 		for _, arg := range args {
-			id, err := strconv.Atoi(arg)
+			//Iterates through each id
+			id, err := strconv.Atoi(arg) // Attempts to parse the id and convert to int
 			if err != nil {
-				fmt.Println("Failed to parse the argument:", arg)
+				fmt.Println("Failed to parse the argument:", arg) //Prints error message if the id fails to be parseed
 			} else {
-				ids = append(ids, id)
+				ids = append(ids, id) // Valid ids are appended to 'ids'
 			}
 		}
-		tasks, err := db.AllTasks()
+		tasks, err := db.AllTasks() // Gives us all the tasks in the database from ALLTasks()
 		if err != nil {
 			fmt.Println("Something went wrong:", err)
 			return
 		}
 		for _, id := range ids {
+			//Iterates over all valid integer ids
 			if id <= 0 || id > len(tasks) {
+				//checks if it is a valid id
 				fmt.Println("Invalid task number:", id)
 				continue
 			}
-			task := tasks[id-1]
-			err := db.DeleteTask(task.Key)
+			err := db.DeleteTask(tasks[id-1].Key) // Deletes the task corresponding to the task id
 			if err != nil {
-				fmt.Printf("Failed to mark \"%d\" as completed. Error: %s\n", id, err)
+				fmt.Printf("Failed to mark \"%d\" as completed. Error: %s\n", id, err) // Prints error message
 			} else {
-				fmt.Printf("Marked \"%d\" as completed.\n", id)
+				fmt.Printf("Marked \"%d\" as completed.\n", id) //Prints on succesful deletion
 			}
 		}
 	},
 }
 
 func init() {
+	/*This code adds the 'do' command using rootCmd */
 	rootCmd.AddCommand(doCmd)
 }
